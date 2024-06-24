36dac42a4f8ff6bd6ea2cafe30d3c52f4c0c1af6
Nason mamu <nasonmamu94@gmail.com>
2024-06-24 19:58:57 +0300

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  // Initialize an empty array to collect employee names
   const employees = [];
 
-  // Iterate through the reportWithIterator using for...of loop
   for (const employee of reportWithIterator) {
     employees.push(employee);
   }
 
-  // Join the array of employee names with '|'
   return employees.join(' | ');
 }
-
