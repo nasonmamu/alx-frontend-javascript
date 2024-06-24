132d905f1d9f3b9b0ae9edd289159406b2059189
Nason mamu <nasonmamu94@gmail.com>
2024-06-24 19:53:48 +0300

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -2,10 +2,14 @@ export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
+
+export function getLast() {
+  return ' is okay';
+}
+
 export function taskNext() {
   let combination = 'But sometimes let';
-  combination += getLast(); // Assuming getLast() is imported or defined
+  combination += getLast();
 
   return combination;
 }
-
