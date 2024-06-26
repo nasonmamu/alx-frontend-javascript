84f42840f2290f0945aedf9d3f9abb991900d19a
Nason mamu <nasonmamu94@gmail.com>
2024-06-26 10:16:52 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
-// 0-promise.js
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulating an asynchronous operation
-    setTimeout(() => {
-      resolve("Success");
-    }, 1000);
-  });
+  return new Promise(() => {});
 }
-
