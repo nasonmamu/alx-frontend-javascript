60ff9be4d07eb0202267a5e26be4b17f78288c06
Nason mamu <nasonmamu94@gmail.com>
2024-07-01 11:19:50 +0300

    Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Nason Mamu <https://github.com/nasonmamu>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
