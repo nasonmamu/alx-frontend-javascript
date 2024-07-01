8a47a840990cd39d861f60107cdf861bcad15d6d
nasonmamu <nasonmamu94@gmail.com>
2024-07-01 01:00:42 -0700

    Tasks

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,15 @@
+etrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Festus Maithya <https://github.com/festusmaithyakcau>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}i
