034a727154bda658a88ff072fbe1196fce6b3c43
Nason mamu <nasonmamu94@gmail.com>
2024-07-02 11:22:21 +0300

    Update 6-set.js

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -3,8 +3,8 @@
  * @param {Set} set - The collection of unique items.
  * @param {*} array - The array of items.
  * @author Nason Mamu <https://github.com/nasonmamu>
- * @returns {Boolean}
+ * @returns {Set}
  */
-export default function hasValuesFromArray(set, array) {
-  return array.every((value) => set.has(value));
+export default function setFromArray(array) {
+  return new Set(array);
 }
