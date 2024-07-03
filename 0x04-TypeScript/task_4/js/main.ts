2b6d13269e98e94e72ed982cc23e9599decfa465
Nason mamu <nasonmamu94@gmail.com>
2024-07-03 15:08:08 +0300

    Update main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,5 +1,4 @@
-
-rt const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
 export const java: Subjects.Java = new Subjects.Java();
 export const react: Subjects.React = new Subjects.React();
 export const cTeacher: Subjects.Teacher = {
