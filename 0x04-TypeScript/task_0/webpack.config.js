4ff2d428e8933061b126ea305dffb733e5eb7393
Nason mamu <nasonmamu94@gmail.com>
2024-07-03 14:51:59 +0300

    Update webpack.config.js

--- 0x04-TypeScript/task_0/webpack.config.js
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -1,4 +1,4 @@
-t path = require("path");
+const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
